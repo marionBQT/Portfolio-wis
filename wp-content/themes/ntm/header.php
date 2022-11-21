dfef6678dad11ff214c645f4b7582491ffcbe9c4
Marion Bouquet <marion.bouquet@epitech.digital>
2022-11-21 13:00:08 +0100

    theme created

---
+++ wp-content/themes/ntm/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="manifest" href="<?php echo get_site_url(); ?>/manifest.json"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <?php wp_head(); ?>
+</head>
+<body>
+<header>
+    <h1><?php bloginfo('name'); ?></h1>
+</header>
